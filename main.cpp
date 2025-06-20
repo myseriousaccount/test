9f46a3ddd3be80044fbbe8426cd031925f5ab725
Yevi <doctor.whoedition@gmail.com>
2025-06-20 22:42:37 +0300

    add print.h and print.cpp files and change main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <sum.h>
 #include <diff.h>
 #include <prod.h>
+#include <print.h>
 using namespace std;
 main ()
 {
@@ -10,7 +11,9 @@ main ()
     int sum = sum(a, b);
     int diff = diff(a, b);
     int prod = prod(a, b);
+    print(a, b);
     cout << "Sum: " << sum << endl;
     cout << "Difference: " << diff << endl;
     cout << "Production: " << prod << endl;
+    
 }
---
+++ print.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void diff (int a, int b) {
+    cout << a << endl;
+    cout << b << endl;
+}
+main ()
+{}
+
